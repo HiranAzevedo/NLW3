a53c064a731c3bf53dd1a5be9dbd638e126ee454
Hiran <hiran.azevedo@vtex.com.br>
2020-10-16 21:46:33 -0300

    add first migration

---
+++ backend/src/database/migrations/1602895508261-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1602895508261 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
